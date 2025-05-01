8637cf7f843eb72685424e01369f72253904c163
Jason Anghad <j.anghad@yahoo.com>
2025-05-01 12:31:01 -0400

    Removed notFound because now the error handling will be redirecting to home

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { notFound, redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import {useEffect, useState} from "react";
